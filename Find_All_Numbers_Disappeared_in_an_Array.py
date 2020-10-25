835b84f46567bbe2f025943dab0dab38d4b6f229
pramod kulkarni <56814988+pramod3009@users.noreply.github.com>
2020-10-25 16:46:07 -0600

    Update Find_All_Numbers_Disappeared_in_an_Array.py

--- Find_All_Numbers_Disappeared_in_an_Array.py
+++ Find_All_Numbers_Disappeared_in_an_Array.py
@@ -8,6 +8,23 @@ class Solution:
             if not d.get(i,0):
                 missing.append(i)
         return missing        
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        for(auto num : nums){
+            if(nums[abs(num) - 1] > 0){
+                nums[abs(num) - 1] = -1*nums[abs(num) - 1];
+            }
             
+        }
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]>0){
+                result.push_back(i+1);
+            }
+        }
+        return result;
+    }
+};
         
         
