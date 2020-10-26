3e902e026c167e9e176a42a03120bdf57a7c5f47
pramod kulkarni <56814988+pramod3009@users.noreply.github.com>
2020-10-26 15:54:36 -0600

    Create countBits.cpp

---
+++ countBits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> dp(num+1, 0);
+        dp[0] = 0;
+        for(int i = 1; i<=num; i++){
+            if(i%2==1){
+                dp[i] = dp[int(i/2)] + 1;
+                
+            } else {
+                dp[i] = dp[int(i/2)];
+            }
+        }
+        return dp;
+    }
+};
