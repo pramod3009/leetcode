4ea5db0a87c9f69f74e76a4704c43b290d1b02f5
pramod kulkarni <56814988+pramod3009@users.noreply.github.com>
2020-11-02 20:00:04 -0700

    Create numRollsToTarget.cpp

---
+++ numRollsToTarget.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRollsToTarget(int d, int f, int target) {
+        vector<vector<int>> dp(d + 1, vector<int>(target + 1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= d; i++){
+            for(int j = 1; j <=target; j++){
+                for(int k = 1; k<=f; k++){
+                    if(j >= k){
+                        dp[i][j] = (dp[i][j] + dp[i-1][j - k] ) % (1000000000 +7);
+                    }
+                }
+            }
+        }
+        return dp[d][target];
+    }
+};
