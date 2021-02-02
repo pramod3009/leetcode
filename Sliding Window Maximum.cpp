149f31ea7d4efc35390c5d38419dda23df49f171
pramod kulkarni <56814988+pramod3009@users.noreply.github.com>
2021-02-02 14:45:39 -0700

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    vector<int> result;
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        if(nums.size()<k){
-            return result;
-        }
-        map<int,int>m;
-        for(int i = 0; i < k; i++){
-            m[nums[i]]++;
-        }
-        result.push_back(m.rbegin()->first);
-        for(int i = k ;i < nums.size() ;i++){
-            m[nums[i-k]]--;
-            if(m[nums[i-k]]==0){
-                m.erase(nums[i-k]);
+        priority_queue<pair<int, int>> q;
+        q.push({nums[0], 0});
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++) {
+            while(!q.empty() and q.top().second <= i - k){
+                q.pop();
             }
-            m[nums[i]]++;
-            result.push_back(m.rbegin()->first);
+            q.push({nums[i], i});
+            if(i >= k-1){
+                result.push_back(q.top().first);
+            }
         }
         return result;
     }
