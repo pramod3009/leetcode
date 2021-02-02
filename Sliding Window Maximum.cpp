9386c7c183e9b536378b3074c412fb42c7534bca
pramod kulkarni <56814988+pramod3009@users.noreply.github.com>
2021-02-02 15:20:06 -0700

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -16,3 +16,25 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++){
+            while(!q.empty() and q.front() <= i - k){
+                q.pop_front();
+            }
+            while(!q.empty() and nums[q.back()] <= nums[i]){
+                q.pop_back();
+            }
+            q.push_back(i);
+            if(i >= k - 1){
+                result.push_back(nums[q.front()]);
+            }
+        }
+        return result;
+    }
+};
